f1e8c600f16df906cc8875b24bf1173b5f72e099
Elliot Seymour <eseymour@nmu.edu>
2021-02-25 18:49:50 -0500

    Adding project to git

--- pyProcesses/makeEq.py
+++
@@ -1,8 +0,0 @@
-import sys
-import numpy
-x = numpy.array(sys.argv[1].split(','), dtype = numpy.int32)
-y = numpy.array(sys.argv[2].split(','), dtype = numpy.int32)
-z = numpy.polyfit(numpy.log(x),y,1)
-z[0] = z[0] * 2.3
-print z
-sys.stdout.flush()
