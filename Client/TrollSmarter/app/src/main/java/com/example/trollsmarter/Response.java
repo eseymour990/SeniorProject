32607c88e2592cc48b3a064faa0c48c9ff81d9e5
eseymour990 <eseymour@nmu.edu>
2021-03-01 21:05:40 -0500

    Added Lure/Trolling device selection in GearActivity

---
+++ Client/TrollSmarter/app/src/main/java/com/example/trollsmarter/Response.java
@@ -0,0 +1,9 @@
+package com.example.trollsmarter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Response {
+    List<HashMap<String, Lure>> Lure_Details = new ArrayList();
+}
